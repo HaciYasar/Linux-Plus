d372878d140ac43cdadc416a5db664e68a9c894c
Hacı YAŞAR <yasar_haci@hotmail.com>
2022-05-21 19:11:05 +0300

    if-else-elif

---
+++ Linux-if-else-elif/ifelse-statement.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+read -p "input a nember " number
+
+if [[ $number -ge 10 ]]
+then
+	  echo "The number is bigger than or equal to 10."
+  else 
+	    echo "The number is smaller than 10"
+fi
